4fa5ee2387929464feebd6dc53d2073e45a82115
Elonm Luc <elonmlucakakpo@gmail.com>
2025-01-21 21:09:57 +0100

    add logout logic

--- users/views.py
+++ users/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegistrationSerializer
-from rest_framework.permissions import AllowAny
-
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 class UserRegistrationView(APIView):
     """
     Vue pour enregistrer un utilisateur standard.
@@ -15,3 +15,19 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response({"message": "Utilisateur créé avec succès."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data.get("refresh_token")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # Blacklist the refresh token
+            
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
