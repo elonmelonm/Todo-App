8fab59220c9a52c7e7a76a28d71c38168140f303
Elonm Luc <elonmlucakakpo@gmail.com>
2025-01-21 19:26:44 +0100

    add toggle-complete-favorite

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TodoListCreateView, TodoDetailView
+from .views import TodoListCreateView, TodoDetailView, ToggleCompleteView, ToggleFavoriteView
 
 urlpatterns = [
     path('', TodoListCreateView.as_view(), name='todo_list_create'),
     path('<uuid:pk>/', TodoDetailView.as_view(), name='todo_detail'),
+    path('<uuid:pk>/toggle-complete/', ToggleCompleteView.as_view(), name='todo_toggle_complete'),
+    path('<uuid:pk>/toggle-favorite/', ToggleFavoriteView.as_view(), name='todo_toggle_favorite'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics, permissions
 from .models import Todo
 from .serializers import TodoSerializer
-
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from .models import Todo
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class TodoListCreateView(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
@@ -26,3 +29,28 @@ class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
+
+class ToggleCompleteView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        todo = get_object_or_404(Todo, id=pk, user=request.user)
+        todo.is_completed = not todo.is_completed
+        todo.save()
+        return Response({
+            "message": f"Task '{todo.title}' marked as {'completed' if todo.is_completed else 'incomplete'}.",
+            "is_completed": todo.is_completed
+        }, status=status.HTTP_200_OK)
+
+
+class ToggleFavoriteView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        todo = get_object_or_404(Todo, id=pk, user=request.user)
+        todo.is_favorite = not todo.is_favorite
+        todo.save()
+        return Response({
+            "message": f"Task '{todo.title}' marked as {'favorite' if todo.is_favorite else 'not favorite'}.",
+            "is_favorite": todo.is_favorite
+        }, status=status.HTTP_200_OK)
