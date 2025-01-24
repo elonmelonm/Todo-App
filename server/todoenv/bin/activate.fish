659e59945326192c6e41512a402bc8c7e4378802
Elonm Luc <elonmlucakakpo@gmail.com>
2025-01-24 17:13:25 +0100

    requirements.txt

--- server/todoenv/bin/activate.fish
+++ server/todoenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/elonm/Documents/dev/todoApp/todo_project/todoenv
+set -gx VIRTUAL_ENV /home/elonm/Documents/dev/todoApp/todo_project/server/todoenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
